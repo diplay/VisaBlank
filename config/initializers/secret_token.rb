5f0f3733a4987f50c76e12e940b42fbe6492a525
diplay <diplaySuper@gmail.com>
2014-04-06 15:16:44 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Visa::Application.config.secret_key_base = 'a01969dbf83003419597f381eee5b5dff2aa1df0bc9b59273507abae85044cadd842b6cbb2efca819241a52760fce4ce25b36fb96b7fc7a68ef3cf6ef893f0c3'
