a88dfebe67ea1e771c6cba54241d9b00b408169e
Levon <diplaySuper@gmail.com>
2014-04-11 14:19:21 +0400

    show company managers tab started

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -2,4 +2,9 @@
 class Manager < ActiveRecord::Base
   belongs_to :company
   has_many :clients
+
+  def month_profit
+    return 0
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+company = Company.create(name: "ООО \"Рога и Копыта\"",
+                        paid_before: Date.today.next_month,
+                        tin: "432134322",
+                        address: "ул. Пушкина, д.23",
+                        bank_details: "Номер счета: 12345",
+                        phone: "788-08-78",
+                        email: "lol@lol.lol")
+manager = company.managers.create(name: "Иванов Иван")
+manager.clients.create(fio: "Лолка Ваня")
