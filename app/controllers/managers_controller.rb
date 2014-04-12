cedaec0bde857e270e9a3af78cd0f0b5ee506322
diplay <diplaySuper@gmail.com>
2014-04-12 17:13:44 +0400

    started managers and added fonts

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,3 +1,18 @@
 #coding: utf-8
 class ManagersController < ApplicationController
+  before_action :check_owner
+
+  def show
+    @manager = Manager.find(params[:id])
+  end
+
+  private
+  def check_owner
+    unless @user.role == "admin" ||
+      (@user.role == "company" && @user.owner.id == params[:company_id].to_i) ||
+      (@user.role == "manager" && @user.owner.id == params[:id].to_i)
+      redirect_to root_path
+    end
+  end
+
 end
