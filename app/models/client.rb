42163473716b48e1a8eebe163982be7177f938f4
diplay <diplaySuper@gmail.com>
2014-04-14 11:58:27 +0400

    client data

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,5 +4,16 @@ class Client < ActiveRecord::Base
   has_one :foreign_passport_data
   has_one :visa_data
   has_many :document_orders
+  after_create :set_documents_data, on: :create
+
+  private
+  def set_documents_data
+    if foreign_passport_data.nil?
+      create_foreign_passport_data
+    end
+    if visa_data.nil?
+      create_visa_data
+    end
+  end
 
 end
