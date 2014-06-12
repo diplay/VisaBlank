a43aec54355bd1fffd181e8c080b54daa1fb922a
alzater <alzater@mail.ru>
2014-06-12 04:44:17 +0400

    need check visa form

--- app/models/document_order.rb
+++ app/models/document_order.rb
@@ -6,15 +6,17 @@ class DocumentOrder < ActiveRecord::Base
 
   def export()
     input_path = self.document_template.template_path
+    output_path = "#{Rails.root}/public/pdfs/DocumentOrder#{self.id}."
+
     case self.document_template.document_type
     when 'visa'
-      output_path = "#{Rails.root}/public/pdfs/DocumentOrder#{self.id}.pdf"
+      output_path += "pdf"
       self.client.visa_data.create_pdf( input_path, output_path)
     when 'foreign_passport'
-      output_path = "#{Rails.root}/public/pdfs/DocumentOrder#{self.id}.pdf"
+      output_path += "pdf"
       self.client.foreign_passport_data.create_pdf( input_path, output_path)
     when 'passport_contract'
-      output_path = "#{Rails.root}/public/pdfs/DocumentOrder#{self.id}.docx"
+      output_path += "docx"
       self.client.gen_passport_contract( input_path, output_path)
     end
     output_path
